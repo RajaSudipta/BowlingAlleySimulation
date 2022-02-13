304db2a48b4589bce87e2df1a853ccc4599a8166
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-02-14 01:56:28 +0530

    removing unused things

--- BowlingAlleySimulation/src/EndGamePrompt.java
+++ BowlingAlleySimulation/src/EndGamePrompt.java
@@ -22,8 +22,6 @@ public class EndGamePrompt implements ActionListener {
 
 	private int result;
 
-	private String selectedNick, selectedMember;
-
 	public EndGamePrompt( String partyName ) {
 
 		result =0;
@@ -47,8 +45,6 @@ public class EndGamePrompt implements ActionListener {
 		// Button Panel
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(1, 2));
-
-		Insets buttonMargin = new Insets(4, 4, 4, 4);
 
 		yesButton = new JButton("Yes");
 		JPanel yesButtonPanel = new JPanel();
