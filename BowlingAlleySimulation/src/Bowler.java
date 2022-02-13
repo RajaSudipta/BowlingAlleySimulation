0fc121199015f36fcb18038d232cb65207fa299c
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-02-14 02:09:48 +0530

    cleaning the 'Bowler' file, removing the 'getNickName' method and 'equals' method (why discussed earlier)

--- BowlingAlleySimulation/src/Bowler.java
+++ BowlingAlleySimulation/src/Bowler.java
@@ -34,13 +34,6 @@ public class Bowler {
   	email = mail;
     }
 
-
-//    public String getNickName() {
-//
-//        return nickName;  
-//
-//    }
-
 	public String getFullName ( ) {
 			return fullName;
 	}
@@ -53,17 +46,4 @@ public class Bowler {
 		return email;	
 	}
 	
-//	public boolean equals ( Bowler b) {
-//		boolean retval = true;
-//		if ( !(nickName.equals(b.getNickName())) ) {
-//				retval = false;
-//		}
-//		if ( !(fullName.equals(b.getFullName())) ) {
-//				retval = false;
-//		}	
-//		if ( !(email.equals(b.getEmail())) ) {
-//				retval = false;
-//		}
-//		return retval;
-//	}
 }
