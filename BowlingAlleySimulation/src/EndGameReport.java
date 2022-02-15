d2e60ed09763aaf7a907e81ddc7107b44097d3fa
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-02-15 22:09:36 +0530

    commenting main method. not invoked

--- BowlingAlleySimulation/src/EndGameReport.java
+++ BowlingAlleySimulation/src/EndGameReport.java
@@ -131,7 +131,7 @@ public class EndGameReport implements ActionListener, ListSelectionListener {
 		win.hide();
 	}
 
-	public static void main( String args[] ) {
+	/*public static void main( String args[] ) {
 		Vector bowlers = new Vector();
 		for ( int i=0; i<4; i++ ) {
 			bowlers.add( new Bowler( "aaaaa", "aaaaa", "aaaaa" ) );
@@ -139,7 +139,7 @@ public class EndGameReport implements ActionListener, ListSelectionListener {
 		Party party = new Party( bowlers );
 		String partyName="wank";
 		EndGameReport e = new EndGameReport( partyName, party );
-	}
+	}*/
 	
 }
 
