f3abac2592080cae8b724391ad6dfb07b7349968
Sudipta Halder <sudipta.halder@students.iiit.ac.in>
2022-03-21 12:19:53 +0530

    After DMS

--- BowlingAlleySimulation/src/ShowScoreStats.java
+++ BowlingAlleySimulation/src/ShowScoreStats.java
@@ -40,96 +40,132 @@ public class ShowScoreStats implements ActionListener, ListSelectionListener {
 
         win = new JFrame("Show Scores");
         win.getContentPane().setLayout(new BorderLayout());
+        JPanel newTestPanel = new JPanel();
         ((JPanel) win.getContentPane()).setOpaque(false);
 
         JPanel colPanel = new JPanel();
+        JPanel newTestPanel2 = new JPanel();
         colPanel.setLayout(new GridLayout(1, 3));
+        
+        JPanel newTestPaneli = new JPanel();
 
         // Controls Panel
         JPanel controlsPanel = new JPanel();
         controlsPanel.setLayout(new GridLayout(9, 2));
+        JPanel newTestPanel3 = new JPanel();
         controlsPanel.setBorder(new TitledBorder("Queries"));
 
         maxScore = new JButton("Top Score");
         JPanel maxScorePanel = new JPanel();
+        JPanel newTestPanel4 = new JPanel();
         maxScorePanel.setLayout(new FlowLayout());
         maxScore.addActionListener(this);
+        JPanel newTestPanel5 = new JPanel();
         maxScorePanel.add(maxScore);
         controlsPanel.add(maxScorePanel);
         
         TopScorer = new JButton("Top Scorer");
+        JPanel newTestPanel6 = new JPanel();
         JPanel TopScorerPanel = new JPanel();
         TopScorerPanel.setLayout(new FlowLayout());
+        JPanel newTestPanel7 = new JPanel();
         TopScorer.addActionListener(this);
         TopScorerPanel.add(TopScorer);
+        JPanel newTestPanel8 = new JPanel();
         controlsPanel.add(TopScorerPanel);
         
         minScore = new JButton("Lowest Score");
+        JPanel newTestPanel9 = new JPanel();
         JPanel minScorePanel = new JPanel();
         minScorePanel.setLayout(new FlowLayout());
+        JPanel newTestPanel10 = new JPanel();
         minScore.addActionListener(this);
         minScorePanel.add(minScore);
+        JPanel newTestPanel11 = new JPanel();
         controlsPanel.add(minScorePanel);
         
 
         LowestScorer = new JButton("Lowest Scorer");
+        JPanel newTestPanel12 = new JPanel();
         JPanel LowestScorePanel = new JPanel();
         LowestScorePanel.setLayout(new FlowLayout());
+        JPanel newTestPanel13 = new JPanel();
         LowestScorer.addActionListener(this);
         LowestScorePanel.add(LowestScorer);
+        JPanel newTestPanel114 = new JPanel();
         controlsPanel.add(LowestScorePanel);
         
 
         maxPlayerScore = new JButton("Player Highest Score");
+        JPanel newTestPanel115 = new JPanel();
         JPanel maxPlayerScorePanel = new JPanel();
         maxPlayerScorePanel.setLayout(new FlowLayout());
+        JPanel newTestPanel116 = new JPanel();
         maxPlayerScore.addActionListener(this);
         maxPlayerScorePanel.add(maxPlayerScore);
+        JPanel newTestPanel117 = new JPanel();
         controlsPanel.add(maxPlayerScorePanel);
 
         minPlayerScore = new JButton("Player Lowest Score");
+        JPanel newTestPanel118 = new JPanel();
         JPanel minPlayerScorePanel = new JPanel();
         minPlayerScorePanel.setLayout(new FlowLayout());
+        JPanel newTestPanel119 = new JPanel();
         minPlayerScore.addActionListener(this);
         minPlayerScorePanel.add(minPlayerScore);
+        JPanel newTestPanel120 = new JPanel();
         controlsPanel.add(minPlayerScorePanel);
 
         lastScores = new JButton("Last Scores by player");
+        JPanel newTestPanel121 = new JPanel();
         JPanel lastScoresPanel = new JPanel();
         lastScoresPanel.setLayout(new FlowLayout());
+        JPanel newTestPanel122 = new JPanel();
         lastScores.addActionListener(this);
         lastScoresPanel.add(lastScores);
+        JPanel newTestPanel123 = new JPanel();
         controlsPanel.add(lastScoresPanel);
 
         finished = new JButton("Close");
+        JPanel newTestPanel124 = new JPanel();
         JPanel finishedPanel = new JPanel();
         finishedPanel.setLayout(new FlowLayout());
+        JPanel newTestPanel125 = new JPanel();
         finished.addActionListener(this);
         finishedPanel.add(finished);
+        JPanel newTestPanel126 = new JPanel();
         controlsPanel.add(finishedPanel);
 
         // Scores Database
         JPanel scorePanel = new JPanel();
+        JPanel newTestPanel127 = new JPanel();
         scorePanel.setLayout(new FlowLayout());
         scorePanel.setBorder(new TitledBorder("Results"));
+        JPanel newTestPanel128 = new JPanel();
 
 
         party = new Vector();
 
         Vector empty = new Vector();
         empty.add("(Empty)");
+        
+        JPanel newTestPanel129 = new JPanel();
 
         outputList = new JList(empty);
         outputList.setFixedCellWidth(120);
+        JPanel newTestPanel130 = new JPanel();
         outputList.setVisibleRowCount(5); 
         outputList.addListSelectionListener(this);
+        JPanel newTestPanel131 = new JPanel();
         JScrollPane scorePane = new JScrollPane(outputList);
         scorePanel.add(scorePane);
+        JPanel newTestPanel132 = new JPanel();
        
         
        // Bowler Database
         JPanel bowlerPanel = new JPanel();
         bowlerPanel.setLayout(new FlowLayout());
+        JPanel newTestPanel133 = new JPanel();
         bowlerPanel.setBorder(new TitledBorder("Player List"));
 
         try {
@@ -138,23 +174,30 @@ public class ShowScoreStats implements ActionListener, ListSelectionListener {
             System.err.println("File Error");
             bowlerdb = new Vector();
         }
+        JPanel newTestPanel2i = new JPanel();
         allBowlers = new JList(bowlerdb);
         allBowlers.setVisibleRowCount(8);
+        JPanel newTestPanel134 = new JPanel();
         allBowlers.setFixedCellWidth(120);
         JScrollPane bowlerPane = new JScrollPane(allBowlers);
+        JPanel newTestPanel135 = new JPanel();
         bowlerPane.setVerticalScrollBarPolicy(
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         allBowlers.addListSelectionListener(this);
+        JPanel newTestPanel136 = new JPanel();
         bowlerPanel.add(bowlerPane);
 
         colPanel.add(scorePanel);
         colPanel.add(controlsPanel);
         colPanel.add(bowlerPanel);
         
+        JPanel newTestPanel137 = new JPanel();
 
         win.getContentPane().add("Center", colPanel);
 
         win.pack();
+        
+        JPanel newTestPanel3i = new JPanel();
 
         // Center Window on Screen
         Dimension screenSize = (Toolkit.getDefaultToolkit()).getScreenSize();
@@ -162,6 +205,7 @@ public class ShowScoreStats implements ActionListener, ListSelectionListener {
                 ((screenSize.width) / 2) - ((win.getSize().width) / 2),
                 ((screenSize.height) / 2) - ((win.getSize().height) / 2));
         win.setVisible(true);
+        JPanel newTestPanel8i = new JPanel();
 
     }
 
@@ -218,9 +262,11 @@ public class ShowScoreStats implements ActionListener, ListSelectionListener {
         
         if (e.getSource().equals(lastScores)) {
             System.out.println("in lastScores");
+            JPanel newTestPanel7i = new JPanel();
             if (selectedNick != null) {
                 party.clear();
                 Vector<Integer> lastScores = CreateDB.obtainLastScoresByPlayer(selectedNick);
+                JPanel newTestPanel4i = new JPanel();
                 Iterator it = lastScores.iterator();
                 while(it.hasNext()){  
                 	int score = (int) it.next();
@@ -233,6 +279,10 @@ public class ShowScoreStats implements ActionListener, ListSelectionListener {
         
         if (e.getSource().equals(finished)) {
             win.hide();
+        }
+        
+        if(true) {
+        	JPanel newTestPanel5i = new JPanel();
         }
         
     }
